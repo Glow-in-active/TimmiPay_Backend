1a08a74b101eaea5a5086be73491a0e76c88439a
Glow-in-active <timurtimurcik7@gmail.com>
2025-04-29 13:50:06 +0300

    починил инклюд

--- internal/user_verify/token_generator/token_generator.cpp
+++ internal/user_verify/token_generator/token_generator.cpp
@@ -1,6 +1,6 @@
 #include "token_generator.h"
 #include "../../storage/user_verify/redis_set/redis_set_token.h"
-#include "../../storage/user_verify/auth/user_verify.cpp"
+#include "../../storage/user_verify/auth/user_verify.h"
 
 std::string TokenGenerator::GenerateToken(const User& user) {
     const std::string token = uuid_gen_.generateUUID();
